dcc0a7d92fb02db69effb0277487064e8d16376e
Gurmanjot Singh Randhawa <gurman@192.168.1.13>
2025-08-30 14:58:29 +0530

    minor

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -66,7 +66,7 @@ const Navigation = () => {
               <motion.button
                 key={item.name}
                 onClick={() => scrollToSection(item.href, item.name)}
-                className="relative text-sm font-medium text-black dark:text-slate-300 hover:text-slate-900 dark:hover:text-white transition-colors"
+                className="relative text-sm font-medium text-slate-900 dark:text-slate-300 hover:text-slate-900 dark:hover:text-white transition-colors"
                 whileHover={isMobile() ? undefined : { y: -2 }}
                 initial={{ opacity: 0, y: -15 }}
                 animate={{ opacity: 1, y: 0 }}
